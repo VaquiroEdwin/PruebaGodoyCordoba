9b1ab4e6968cf786e4ff793fc3f4eee8aa8984dd
VaquiroEdwin <edwinsandoval090@gmail.com>
2025-01-28 21:26:53 -0500

    Se aplican correcciones a la logica del negocio del API .net core MVC

--- ApiPruebaTecnica/ApiPruebaTecnica/Controllers/UsuariosController.cs
+++ ApiPruebaTecnica/ApiPruebaTecnica/Controllers/UsuariosController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ApiPruebaTecnica.Modelo;
+using System.Net;
 
 namespace ApiPruebaTecnica.Controllers
 {
@@ -14,108 +15,153 @@ namespace ApiPruebaTecnica.Controllers
     public class UsuariosController : ControllerBase
     {
         private readonly GodoyCordobaContext _context;
+        private readonly ILogger<UsuariosController> _logger;
 
-        public UsuariosController(GodoyCordobaContext context)
+        public UsuariosController(GodoyCordobaContext context, ILogger<UsuariosController> logger)
         {
             _context = context;
+            _logger = logger;   
         }
 
         // GET: api/Usuarios
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
+        public async Task<ActionResult<IEnumerable<Usuario>>> ListarUsuarios()
         {
-            return await _context.Usuarios.ToListAsync();
+            try
+            {
+                var ListaUsuarios = await _context.Usuarios.ToListAsync();
+                if (ListaUsuarios == null)
+                {
+                    return NotFound("No hay usuarios registrados, por favor registre usuarios");
+                }       
+                return Ok(ListaUsuarios);
+            }
+            catch(Exception error)
+            {
+                _logger.LogError(error, "Error al obtener lista de usuarios");
+                return StatusCode(500, "Ocurrió un error interno al procesar la solicitud, por favor vuelve a intentarlo");
+            }
         }
 
         // GET: api/Usuarios/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Usuario>> GetUsuario(long id)
+        public async Task<ActionResult<Usuario>> ListarUsuarioBYId(long id)
         {
-            var usuario = await _context.Usuarios.FindAsync(id);
-
-            if (usuario == null)
-            {
-                return NotFound();
-            }
-
-            return usuario;
-        }
-
-        // PUT: api/Usuarios/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutUsuario(long id, Usuario usuario)
-        {
-            if (id != usuario.Cedula)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(usuario).State = EntityState.Modified;
-
             try
             {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UsuarioExists(id))
+                var usuario = await _context.Usuarios.FindAsync(id);
+                if (usuario == null)
                 {
-                    return NotFound();
+                    return NotFound($"El usuario con cédula {id} no existe en el sistema, por favor valide");
                 }
-                else
-                {
-                    throw;
-                }
-            }
 
-            return NoContent();
+                return Ok(usuario);
+            }
+            catch(Exception error)
+            {
+                // Log para tener en cuenta los registros y posibles excepciones
+                _logger.LogError(error, "Error al obtener el usuario con ID {Id}", id);
+
+                // Return a 500 Internal Server Error with a generic error message
+                return StatusCode(500, "Ocurrió un error interno al procesar la solicitud, por favor vuelve a intentarlo");
+            }
+           
         }
 
-        // POST: api/Usuarios
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //crear usuario en la tabla Usuarios
         [HttpPost]
-        public async Task<ActionResult<Usuario>> PostUsuario(Usuario usuario)
+        public async Task<IActionResult> CrearUsuario(Usuario usuario)
         {
-            _context.Usuarios.Add(usuario);
             try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateException)
             {
                 if (UsuarioExists(usuario.Cedula))
                 {
-                    return Conflict();
+                    return Conflict($"El usuario con la cédula {usuario.Cedula} ya existe, si desea cambiar algún dato por favor actualice el usuario");
                 }
-                else
-                {
-                    throw;
-                }
-            }
 
-            return CreatedAtAction("GetUsuario", new { id = usuario.Cedula }, usuario);
+                usuario.Puntaje = usuario.CalcularPuntaje();  
+                usuario.FechaAcceso = DateTime.Now; 
+
+                _context.Usuarios.Add(usuario);
+                await _context.SaveChangesAsync();
+                return Ok("Usuario creado con éxito");
+            }
+            catch (Exception error)
+            {
+                _logger.LogError(error, "Error al ecrear un usuario en la tabla de la base de datos.");
+                return StatusCode(500, "Ocurrió un error interno al procesar la solicitud, por favor vuelve a intentarlo");
+            }
         }
 
-        // DELETE: api/Usuarios/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteUsuario(long id)
+        //Editar un usuario existente en la tabla Usuarios
+        [HttpPost]
+        public async Task<IActionResult> EditarUsuario(Usuario usuarioNuevo)
         {
-            var usuario = await _context.Usuarios.FindAsync(id);
-            if (usuario == null)
+            try
             {
-                return NotFound();
+                var usuarioExistente = await _context.Usuarios.FindAsync(usuarioNuevo.Cedula);       
+
+                if(usuarioExistente == null)
+                {
+                    return NotFound($"El usuario con cédula {usuarioNuevo.Cedula} no existe en el sistema, por favor valide");       
+                }
+
+                usuarioNuevo.Puntaje = usuarioNuevo.CalcularPuntaje();       
+                usuarioNuevo.FechaAcceso = usuarioExistente.FechaAcceso;  // respetar la fecha de acceso que tenía el usuario  
+                
+                _context.Usuarios.Update(usuarioNuevo);  
+
+                await _context.SaveChangesAsync();
+
+                return Ok("Usuario creado con éxito");
             }
+            catch (Exception error)
+            {
+                _logger.LogError(error, "Error al ecrear un usuario en la tabla de la base de datos.");
+                return StatusCode(500, "Ocurrió un error interno al procesar la solicitud, por favor vuelve a intentarlo");
+            }
+        }
 
-            _context.Usuarios.Remove(usuario);
-            await _context.SaveChangesAsync();
+        // borrar usuario en la tabla Usuarios por Id = Cedula
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> BorrarUsuario(long id)
+        {
+            try
+            {
+                var usuario = await _context.Usuarios.FindAsync(id);
+                if (usuario == null)
+                {
+                    return NotFound("El usuario que se desea eliminar no existe en la base de datos");
+                }
 
-            return NoContent();
+                _context.Usuarios.Remove(usuario);
+                await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+            catch(Exception error)
+            {
+                _logger.LogError(error, "Error al eliminar un usuario en la tabla de la base de datos.");
+                return StatusCode(500, "Ocurrió un error interno al procesar la solicitud, por favor vuelve a intentarlo"); 
+            }
+           
         }
 
         private bool UsuarioExists(long id)
         {
-            return _context.Usuarios.Any(e => e.Cedula == id);
+            try
+            {
+                var ExistenciaUsuario = _context.Usuarios.Any(e => e.Cedula == id);
+                return ExistenciaUsuario;
+            }
+            catch (Exception error)
+            {
+                _logger.LogError(error, "Error al comprobar la existencia de un usuario en la tabla de la base de datos.");
+                return false;
+            }
+         
         }
+
+
     }
 }
---
+++ ApiPruebaTecnica/ApiPruebaTecnica/Models/Usuario.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApiPruebaTecnica.Modelo;
+
+public partial class Usuario
+{
+    public long Cedula { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string Apellido { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public DateTime FechaAcceso { get; set; }
+
+    public long Puntaje { get; set; }
+
+    public long CalcularPuntaje()
+    {
+        var CaracteresNombre = $"{Nombre} {Apellido}".Length;
+        int puntaje;      
+
+        if(CaracteresNombre > 10)
+        {
+            puntaje = +20;
+        }
+        else if(CaracteresNombre > 5)
+        {
+            puntaje = +10;  
+        }
+
+        if(Email.Equals("gmail.com", StringComparison.OrdinalIgnoreCase))
+        {
+            puntaje = +40;   
+        }
+        else if(Email.Equals("hotmail.com",StringComparison.OrdinalIgnoreCase))
+        {
+            puntaje = +30;  
+        }   
+        else
+        {
+            puntaje = +10;  
+        }
+
+
+        return puntaje; 
+    }
+
+}
